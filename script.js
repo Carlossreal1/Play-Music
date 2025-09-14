c5bc786e9b8a7a3a675bca28f088395d21224000
Carlossreal1 <joseccrodrigues@hotmail.com>
2025-09-14 21:51:13 +0200

    Play music

---
+++ script.js
@@ -0,0 +1,7 @@
+const songName = document.getElementById("song-name");
+
+const song = document.getElementById("audio");
+
+songName.innerText = "01. Wanna Be Startin' Somethin'.mp3";
+
+song.play();
